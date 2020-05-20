d49c7625a520ed128be0f7772c66ca11afb271e3
jrosiak <61691629+jrosiak@users.noreply.github.com>
2020-05-20 11:55:42 +0200

    Add files via upload

---
+++ TUT_10_3/Person.java
@@ -0,0 +1,15 @@
+package TUT_10_3;
+
+public class Person {
+    String name;
+    String surname;
+    int birthYear;
+    int currentYear;
+    int age;
+
+    public int getAge(){
+        return age;
+        }
+    }
+
+
