424ba9a708b48bb4532a39358b43812f69918e45
jrosiak <61691629+jrosiak@users.noreply.github.com>
2020-05-28 18:04:43 +0200

    Update Main.java

--- TUT_11_4/Main.java
+++ TUT_11_4/Main.java
@@ -5,13 +5,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        NegativeNumberException e = new NegativeNumberException();
-        try {
-            e.readNumber();
-        }catch (Exception exception){
-            System.out.println("Negative numbers are not allowed");
-        }
-        e.fillArray();
+        readNumber();
+        fillArray();
 
     }
+
+    static Scanner input = new Scanner(System.in);
+    static int i;
+    static int j;
+    static int array[] = new int[10];
+
+    public static int readNumber() throws NegativeNumberException{
+        System.out.println("Please provide a number");
+        i = input.nextInt();
+        if (i < 0) {
+            throw new NegativeNumberException("Negative numbers are not allowed");
+        }else{
+            return i;
+        }
+    }
+
+    public static void fillArray(){
+        if(i<0){
+            for (int y : array) {
+                array[y] = 0;
+            }
+        }else{
+            System.out.println("Provide the desired index");
+            j=input.nextInt();
+            //This part makes the output more user friendly, so that index equals the position in the output
+            //e.g. 4th index is the 4th number in the output not 5th as it would be in the normal array where the first index is 0
+
+            if(j<1){
+                throw new IllegalArgumentException("Index cannot be lass that 1!!!");
+            }
+            array[j-1]=i;
+        }
+        for(int i=0; i<10; i++){
+            System.out.print(array[i]);
+            System.out.print(" ");
+        }
+    }
+
 }
+
